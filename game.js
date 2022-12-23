ff8b6f4d202512cc2af95a67824acd452d283834
C Wagner <christopher.wagner@klg.muenchen.musin.de>
2022-12-23 18:46:47 +0100

    Multiple characters in guess wont be marked multiple

--- game.js
+++ game.js
@@ -1,7 +1,7 @@
 class Game {
-    game_over;
-    remaining_tries;
-    word;
+    game_over
+    remaining_tries
+    word
 
     constructor() {
         this.reset()
@@ -19,15 +19,43 @@ class Game {
 
     guess(guess) {
         var characters = new Array(guess.length)
+
+        // set every character to "grey"
         for (var i = 0; i < this.word.length; i++) {
-            if (this.word.charAt(i) == guess.toUpperCase().charAt(i)) {
+            characters[i] = -1
+        }
+        
+        // count how often every character is in the word
+        var wordCharCount = {}
+        for (var i = 0; i < this.word.length; i++) {
+            wordCharCount[this.word.charAt(i)] = 0
+        }
+        for (var i = 0; i < this.word.length; i++) {
+            wordCharCount[this.word.charAt(i)]++
+        }
+
+        // set right characters to "green"
+        // and decrease the characters count to mark it as "used"
+        for (var i = 0; i < this.word.length; i++) {
+            var wChar = this.word.charAt(i)
+            var gChar = guess.toUpperCase().charAt(i)
+            if (wChar == gChar) {
                 characters[i] = 1
-            } else if (this.word.includes(guess.toUpperCase().charAt(i))) {
-                characters[i] = 0
-            } else {
-                characters[i] = -1
+                wordCharCount[gChar]--
             }
         }
+
+        // set right characters at the wrong position to "yellow"
+        // if they are not already green (= "used")
+        for (var i = 0; i < this.word.length; i++) {
+            var wChar = this.word.charAt(i)
+            var gChar = guess.toUpperCase().charAt(i)
+            if (this.word.includes(gChar) && wordCharCount[gChar] > 0) {
+                console.log(i + gChar)
+                characters[i] = 0
+            }
+        }
+
         return characters;
     }
 }
